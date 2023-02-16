0e064fc23859bca5587e77753b5426895e8389e1
Rikki Schulte <rikki.schulte@gmail.com>
2023-02-16 21:53:58 +0100

    working example of monaco & next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,48 @@
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const transpileModules = require("next-transpile-modules");
+
+// const TM = transpileModules(["monaco-editor"], {
+//  resolveSymlinks: true,
+//  debug: true,
+// });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  transpilePackages: ["monaco-editor"],
+  webpack(config, { isServer }) {
+    config.output.publicPath = "/_next/";
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "./node_modules/monaco-editor/esm/vs/base/common/marked/marked.js":
+        "marked",
+      "../common/marked/marked.js": "marked",
+    };
+    config.plugins.push(
+      new MonacoWebpackPlugin({
+        languages: ["json", "graphql"],
+        filename: "static/[name].worker.js",
+        // publicPath: 'static'
+      //  publicPath: "/static/"
+        // globalAPI: true,
+      })
+    );
 
-module.exports = nextConfig
+   // console.log(require.resolve("monaco-graphql/esm/graphql.worker.js"))
+    console.log(config.module.rules)
+    config.module.rules.push({
+      test: require.resolve("monaco-graphql/esm/graphql.worker.js"),
+      use: {
+        loader: "worker-loader",
+        options: {
+          filename: "static/graphql.worker.js",
+          publicPath: '/_next/'
+        },
+      },
+    });
+    // config.output.globalObject = 'self'
+    return config;
+  },
+};
+
+module.exports = nextConfig;
